32030b8f16322e159941f4c4186b218d01b5eb0c
Tristan Armitage <armitageth@gmail.com>
2024-12-09 12:14:54 -0500

    Updated second stage of dictionary generation

--- qathebo/make_dictionary.sh
+++ qathebo/make_dictionary.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
-cut ./dictionary/raw.tsv -f 2 | tail --lines=+2 > ./lexicon/dict_raw.wli
-lexurgy sc -o modern ./lexicon/lex.lsc ./lexicon/dict_raw.wli
-rename raw_modern modern ./lexicon/dict_raw_modern.wl*
-lexurgy sc -o adlam ./lexicon/adlam.lsc ./lexicon/dict_modern.wli
-rename modern_adlam adlam ./lexicon/dict_modern_adlam.wl*
-paste ./lexicon/dict_adlam.wli ./lexicon/dict_modern.wli | cat <(paste <(echo "Adlam") <(echo "Phonetic")) - | paste - <(cut -f 3-5 ./dictionary/raw.tsv) > ./dictionary/adlam.tsv
-sort ./dictionary/adlam.tsv > ./dictionary/sorted.tsv
+cat ./dictionary/sorted.tsv > ./dictionary/dictionary.tsv
+pandoc -s -o ./html/dictionary.html ./dictionary/dictionary.tsv --metadata title="Qʼathebʼo Dictionary"
