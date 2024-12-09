01b1f64e592c4ffb1371f0e15130e8b8534d4f3f
Tristan Armitage <armitageth@gmail.com>
2024-12-09 12:04:11 -0500

    Changed how dictionary is generated

--- qathebo/make_dictionary.sh
+++ qathebo/make_dictionary.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
-cut ./dictionary/unsorted_dictionary.tsv -f 2 | tail --lines=+2 > ./lexicon/dict.wli
-lexurgy sc -o adlam ./lexicon/adlam.lsc ./lexicon/dict.wli
-cat <(echo "Adlam") ./lexicon/dict_adlam.wli | paste - <(cut -f 2-5 ./dictionary/unsorted_dictionary.tsv) > ./dictionary/auto_adlam.tsv
-sort ./dictionary/auto_adlam.tsv > ./dictionary/sorted_dictionary.tsv
+cut ./dictionary/raw.tsv -f 2 | tail --lines=+2 > ./lexicon/dict_raw.wli
+lexurgy sc -o modern ./lexicon/lex.lsc ./lexicon/dict_raw.wli
+rename raw_modern modern ./lexicon/dict_raw_modern.wl*
+lexurgy sc -o adlam ./lexicon/adlam.lsc ./lexicon/dict_modern.wli
+rename modern_adlam adlam ./lexicon/dict_modern_adlam.wl*
+paste ./lexicon/dict_adlam.wli ./lexicon/dict_modern.wli | cat <(paste <(echo "Adlam") <(echo "Phonetic")) - | paste - <(cut -f 3-5 ./dictionary/raw.tsv) > ./dictionary/adlam.tsv
+sort ./dictionary/adlam.tsv > ./dictionary/sorted.tsv
