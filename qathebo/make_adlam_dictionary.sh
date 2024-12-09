cdda51962771d763f063dfbd7fa8736b106f85e7
Tristan Armitage <armitageth@gmail.com>
2024-12-09 09:59:47 -0500

    Expanded automated dictionary generation

--- qathebo/make_adlam_dictionary.sh
+++ qathebo/make_adlam_dictionary.sh
@@ -2,3 +2,4 @@
 cut ./dictionary/unsorted_dictionary.tsv -f 2 | tail --lines=+2 > ./lexicon/dict.wli
 lexurgy sc -o adlam ./lexicon/adlam.lsc ./lexicon/dict.wli
 cat <(echo "Adlam") ./lexicon/dict_adlam.wli | paste - <(cut -f 2-5 ./dictionary/unsorted_dictionary.tsv) > ./dictionary/auto_adlam.tsv
+sort ./dictionary/auto_adlam.tsv > ./dictionary/sorted_dictionary.tsv
