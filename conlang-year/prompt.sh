dd2b1a802bf631ca09fb1f43bfcc91f92d736da9
Tristan Armitage <armitageth@gmail.com>
2025-02-01 18:25:13 -0500

    Created script for getting prompt based on a given day

---
+++ conlang-year/prompt.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+CYDATE=$(date -d "2024-1-1 +$(($1 - 1)) days" +%F)
+CYSLASHED=$(echo $CYDATE | sed 's/-/\//g')
+CYEXPANDED=$(date -d $CYDATE +%B-%-d-%Y)
+echo "https://www.quothalinguist.com/$CYSLASHED/day-$1-$CYEXPANDED"
