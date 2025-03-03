5f586f4102f4e682bb85f4efc270bade699fe8ea
Tristan Armitage <armitageth@gmail.com>
2025-03-03 14:40:21 -0500

    Improved date updating for make_grammar.sh

--- speedlang23/make_grammar.sh
+++ speedlang23/make_grammar.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+METADATA_DATE=$(date +"%-d %B %Y")
+sed -i "/^date/c\date: '$METADATA_DATE'" ./metadata_grammar.yaml
 cat <(echo "<style>") ./template.css <(echo "</style>") ./grammar.md | pandoc -s -o ./temp.html --metadata-file="./metadata_grammar.yaml"
 awk -f ./generate_numbered_glosses.awk ./temp.html > ./grammar.html
 rm ./temp.html
